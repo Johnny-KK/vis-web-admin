ff94455d218d7d7ff0c087a5441cd9d549135601
johnny <1249864985@qq.com>
2021-02-10 08:14:26 +0000

    新增docker打包配置

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
+app_name="vis-web-admin"
+app_version="0.0.1"
+
+echo "node version: \c"
+node -v
+echo "npm version: \c"
+npm -v
+echo "vue version: \c"
+vue -V
+echo "docker version: \c"
+docker -v
 npm run build
-docker build -t vis-web-admin:0.0.1 .
+docker build -t ${app_name}:${app_version} .
+docker image ls ${app_name}
