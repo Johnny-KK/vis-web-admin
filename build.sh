d75eaf6141198e3bf7277802d4d8f968ea68e6a9
johnny <1249864985@qq.com>
2021-02-18 06:36:18 +0000

    打包配置修改

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 app_name="vis-web-admin"
-app_version="0.0.1"
+app_version="0.0.2"
+docker_user="vis"
+docker_pwd="visPower@2021"
+docker_project="vis"
+docker_domain="ub20-docker.com"
 
 echo "node version: \c"
 node -v
@@ -9,6 +13,13 @@ echo "vue version: \c"
 vue -V
 echo "docker version: \c"
 docker -v
+
 npm run build
-docker build -t ${app_name}:${app_version} .
+
+docker login --username=${docker_user} --password=${docker_pwd} ${docker_domain}
+
+docker build -t ${docker_domain}/${docker_project}/${app_name}:${app_version} .
+
 docker image ls ${app_name}
+
+docker push ${docker_domain}/${docker_project}/${app_name}:${app_version}
